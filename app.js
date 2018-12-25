aff58d2b80ee7a9c58de04fb5c191df8ee6a591c
Dmitry Zaytsev <dmitry.zaytsev83@gmail.com>
2018-12-25 19:38:37 +0300

    add removeBook, addBook, clearFields

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 class Book {
     constructor(_title, _author, _isbn) {
         this.title = _title;
-        this._author = _author;
+        this.author = _author;
         this.isbn = _isbn;
     }
 }
+
 // UI Class Handle UI Tasks
 class UI {
     static displayBooks() {
@@ -24,6 +25,7 @@ class UI {
 
         books.forEach(book => UI.addBookToList(book));
     }
+
     static addBookToList(book) {
         const list = document.querySelector('#book-list');
         const row = document.createElement('tr');
@@ -35,11 +37,47 @@ class UI {
         `;
         list.appendChild(row);
     }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
 }
+
 // Store Class: Handles storage
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Add book to UI
+    UI.addBookToList(book);
+
+    // Clear Fields
+    UI.clearFields();
+});
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+});
